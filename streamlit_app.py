9fa5f668bc4f79aeb9d5fd8c4b1689cac0517554
ssafyguy <ssafyguy@gmail.com>
2023-07-17 17:18:13 +0900

    py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,19 @@ st.write(msg)  # 문자열 직접 넣어도 상관 없음 (streamlit -> print?)
 # 터미널(git bash, cmd) : pip install streamlit (ctrl+`)
 
 st.write(math.pi)
+
+# 원주율
+PI = math.pi
+# 반지름
+radius = 15
+
+PI_is = '원주율 : '
+radius_is = '반지름 : '
+circum = '원의 둘레 : '
+area = '원의 넓이 : '
+
+st.write(PI_is, PI)
+st.write(radius_is, radius)
+st.write(circum, radius * 2 * PI)
+st.write(area, radius * radius * PI)
+st.write(area, radius ** 2 * PI)

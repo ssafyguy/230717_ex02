477d0706596b64ddadcfe3048e6fcb090e044d10
ssafyguy <ssafyguy@gmail.com>
2023-07-17 17:14:37 +0900

    st.write(math.pi)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import math
 
 msg = "Hello World!!!"
 st.write(msg)  # 문자열 직접 넣어도 상관 없음 (streamlit -> print?)
 
 # 터미널(git bash, cmd) : pip install streamlit (ctrl+`)
+
+st.write(math.pi)
